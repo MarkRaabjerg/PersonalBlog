7f0406abcf6e5471d149d243564f0853e6d898e2
MarkRaabjerg <markraabjerg@live.dk>
2026-09-15 22:55:58 +0200

    Update PocketBase URL to point to local network address

--- nextjs_website/lib/pocketbase.ts
+++ nextjs_website/lib/pocketbase.ts
@@ -1,5 +1,11 @@
 import PocketBase from "pocketbase";
 
-export const pb = new PocketBase(
-  "http://127.0.0.1:8090"
-);
+export const pb = new PocketBase("http://192.168.0.15:8090");
+
+
+
+// import PocketBase from "pocketbase";
+
+// export const pb = new PocketBase(
+//   "http://127.0.0.1:8090"
+// );
